38320f3620015034684a479af6f39eb589b908b6
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2024-04-16 12:42:40 +0800

    fixed footer and navbar ui

--- app/components/layout/Footer.js
+++ app/components/layout/Footer.js
@@ -3,12 +3,26 @@ import Image from "next/image";
 
 function Footer() {
   return (
-    <div className="bg-[#F5F4F0] w-full flex flex-col">
-      <div className="flex flex-row justify-between items-center">
-        <Image />
-        <div className="flex flex-row items-center"></div>
+    <div className="bg-[#F5F4F0] w-full pt-28 px-7 sm:px-20">
+      <div className="mx-auto rounded-t-3xl">
+        <div className="flex flex-col lg:flex-row gap-16 lg:gap-0 justify-between items-start lg:items-center container">
+          <Image src={"/geni-logo.png"} width={96} height={26} alt="logo" />
+          <div className="flex flex-col lg:flex-row lg:items-center gap-4 lg:gap-40 text-[#2D262D]">
+            <div className="flex flex-row items-center gap-24">
+              <span>Geni Creators</span>
+              <span>Geni Brands</span>
+            </div>
+            <div className="flex flex-row gap-4 items-center">
+              <Image src={"/ig-icon.png"} width={40} height={40} alt="logo" />
+              <Image src={"/mail-icon.png"} width={40} height={40} alt="logo" />
+            </div>
+          </div>
+        </div>
+        <div className="mt-8 lg:mt-24 flex flex-row text-sm text-[#2D262D] w-full justify-between items-center py-10 border-t-[1px] border-[#2D262D]">
+          <span>Terms and conditions</span>
+          <span>2024 - All rights reserved</span>
+        </div>
       </div>
-      <div className="flex flex-row justify-between items-center py-10 border-t-[1px] border-[#2D262D]"></div>
     </div>
   );
 }
--- app/components/layout/Navbar.js
+++ app/components/layout/Navbar.js
@@ -3,18 +3,27 @@ import Image from "next/image";
 
 function Navbar() {
   return (
-    <div className="w-full bg-[#F5F4F0] top-0 absolute py-[52px]">
-      <div className="relative flex flex-row w-full justify-center items-center gap-12 text-base text-[#000000B8]">
+    <div className="w-full bg-[#F5F4F0] top-0 absolute py-[52px] px-7 sm:px-20">
+      <div className="relative flex flex-row w-full justify-between sm:justify-center items-center gap-12 text-base text-[#000000B8]">
         <Image
           src={"/geni-logo.png"}
           height={26}
           width={96.2}
           alt="logo"
-          className="absolute left-[84px]"
+          className="static sm:absolute left-0"
         />
-        <a>Geni Creators</a>
-        <span className="font-bold">·</span>
-        <a>Geni Brands</a>
+        <a className="hidden sm:block">Geni Creators</a>
+        <span className="font-bold hidden sm:block">·</span>
+        <a className="hidden sm:block">Geni Brands</a>
+        <button>
+          <Image
+            src={"/menu-icon.png"}
+            width={24}
+            height={24}
+            alt="menu"
+            className="block sm:hidden"
+          />
+        </button>
       </div>
     </div>
   );
