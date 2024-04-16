9e34a05652f95f4b5c016c50d1ca9e2a517a0a6c
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2024-04-17 00:39:23 +0800

    updated

---
+++ app/api/getContents.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "http://0.0.0.0:8055/Items/content?sort=sort,-date_created&fields=*,Brand.*.*,Creator.*.*,Category.*.*&filter=%7B%22status%22:%7B%22_eq%22:%22published%22%7D%7D"
+    );
+    if (!response.ok) {
+      throw new Error("Response not ok");
+    }
+    const data = await response.json();
+    res.status(200).json({ data });
+  } catch (error) {
+    console.error("Error fetching API data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
