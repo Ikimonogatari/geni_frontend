3b5a5bcdbdac30ad6c4c61b45a42108287356feb
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2024-04-22 16:46:13 +0800

    updated

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <link rel="icon" href="/favicon.ico" sizes="any" />
+      <meta property="og:image" content="/geni-logo.svg" />
       <Head>
         <title>GENI</title>
       </Head>
