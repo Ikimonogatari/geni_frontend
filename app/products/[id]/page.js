1f6abb160244f1c657eebed43b37ca1154202cf0
トゥブシン ［ＤＤＡＭ］ Tuvshinjargal Erdenebaatar <e.tuvshinjargal@mn.data-artist.com>
2024-10-10 12:03:47 +0800

    fixed product list brand profile picture

--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -196,7 +196,7 @@ function Page() {
             <div className="flex flex-col gap-4 w-full md:w-1/2">
               <div className="flex flex-row items-center gap-3">
                 <Image
-                  src={getPublicProductByIdData?.BrandProfileLink}
+                  src={getPublicProductByIdData?.BrandProfileUrl}
                   width={77}
                   height={77}
                   alt=""
