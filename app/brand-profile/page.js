8290b1b29c4ed9080efdc7668ee805fddb5706d3
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2024-10-24 14:35:32 +0800

    shortened add product text

--- app/brand-profile/page.js
+++ app/brand-profile/page.js
@@ -311,7 +311,7 @@ function page() {
               href="/add-product"
               className="w-full max-w-[270px] hidden md:flex flex-row text-[10px] sm:text-base items-center gap-2 bg-[#4D55F5] border-[1px] border-[#2D262D] px-3 sm:px-5 py-2 sm:py-3 rounded-lg text-white font-bold"
             >
-              Шинэ бүтээгдэхүүн нэмэх
+              Бүтээгдэхүүн нэмэх
               <Image src={"/add-icon.png"} width={14} height={14} alt="arrow" />
             </a>
           </div>
