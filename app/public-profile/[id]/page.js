43788678307a17228b7312037e0ffc022acc4d60
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2024-10-26 18:03:10 +0800

    added public product page

---
+++ app/public-profile/[id]/page.js
@@ -0,0 +1,183 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import {
+  useGetUserInfoQuery,
+  useListContentGalleryQuery,
+  useListCreatorContentsQuery,
+} from "@/app/services/service";
+import Link from "next/link";
+import ContentGallery from "@/app/components/ContentGallery";
+
+function page() {
+  const {
+    data: listContentGalleryData,
+    error: listContentGalleryError,
+    isLoading: listContentGalleryLoading,
+  } = useListContentGalleryQuery();
+  const {
+    data: getUserInfoData,
+    error: getUserInfoError,
+    isLoading: getUserInfoLoading,
+  } = useGetUserInfoQuery();
+  const {
+    data: listCreatorContentsData,
+    error: listCreatorContentsError,
+    isLoading: listCreatorContentsLoading,
+  } = useListCreatorContentsQuery();
+
+  const instagramLink = getUserInfoData?.SocialChannels?.find(
+    (channel) => channel.PlatformName === "Instagram"
+  );
+
+  const facebookLink = getUserInfoData?.SocialChannels?.find(
+    (channel) => channel.PlatformName === "Facebook"
+  );
+  return (
+    <div className="min-h-screen w-full h-full bg-white">
+      <div className="pt-32 pb-16 sm:pb-24">
+        <div className="container text-[#2D262D] max-w-7xl min-h-screen mx-auto px-7 py-10 sm:py-20">
+          <div className="flex flex-col sm:flex-row gap-4 sm:gap-0 items-start justify-between w-full">
+            <div className="flex flex-row items-center gap-7">
+              {getUserInfoData ? (
+                <Image
+                  src={
+                    getUserInfoData?.ProfileLink
+                      ? getUserInfoData?.ProfileLink
+                      : "/dummy-profile.jpg"
+                  }
+                  width={194}
+                  height={194}
+                  loading="lazy"
+                  className="rounded-full sm:rounded-xl w-[90px] h-[90px] sm:w-[194px] sm:h-[194px] aspect-square border border-[#2D262D]"
+                  alt=""
+                />
+              ) : (
+                <div className="w-[90px] h-[90px] sm:w-[194px] sm:h-[194px]"></div>
+              )}
+              <div className="flex flex-col gap-1 sm:gap-2">
+                <div className="flex flex-row items-center gap-3">
+                  <span className="text-[#2D262D] text-base sm:text-2xl font-bold">
+                    {getUserInfoData?.FirstName} {getUserInfoData?.LastName}
+                  </span>
+
+                  <Image
+                    src={"/verified-icon.png"}
+                    width={24}
+                    height={24}
+                    alt="verified-icon"
+                    className="w-4 h-4 sm:w-6 sm:h-6"
+                  />
+                </div>
+                <span className="text-[#6F6F6F] text-xs sm:text-base">
+                  @{getUserInfoData?.Nickname}
+                </span>
+                <div className="flex flex-row items-center gap-2 sm:gap-3">
+                  <span className="text-sm sm:text-lg">
+                    {getUserInfoData ? getUserInfoData.Point : 0} xp
+                  </span>
+                  <a>
+                    <Image
+                      src={"/Info.png"}
+                      width={24}
+                      height={24}
+                      alt="info"
+                      className="w-4 h-4 sm:w-6 sm:h-6"
+                    />
+                  </a>
+                  {instagramLink && (
+                    <a
+                      target="_blank"
+                      href={`https://www.instagram.com/${instagramLink || ""}`}
+                    >
+                      <Image
+                        src={"/Instagram.png"}
+                        width={24}
+                        height={24}
+                        alt="ig"
+                        className="w-5 h-5 sm:w-6 sm:h-6"
+                      />
+                    </a>
+                  )}
+                  {facebookLink && (
+                    <a
+                      target="_blank"
+                      href={`https://www.facebook.com/${facebookLink || ""}`}
+                    >
+                      <Image
+                        src={"/Facebook.png"}
+                        width={24}
+                        height={24}
+                        alt="fb"
+                        className="w-5 h-5 sm:w-6 sm:h-6"
+                      />
+                    </a>
+                  )}
+                </div>
+                <span className="text-[#6F6F6F] text-xs sm:text-base">
+                  {getUserInfoData ? getUserInfoData.Bio : ""}
+                </span>
+                <div className="mt-1 flex flex-wrap items-center gap-2 text-[#2D262D]">
+                  {getUserInfoData?.Category?.map((c, i) => (
+                    <button
+                      key={i}
+                      className="bg-[#CA7FFE] font-semibold rounded-full px-4 py-2"
+                    >
+                      {c.Category_id.name}
+                    </button>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row items-center gap-2 sm:gap-5">
+              <Link
+                href={"/notifications"}
+                className="border-[#2D262D] bg-[#F5F4F0] p-2 gap-5 rounded-lg"
+              >
+                <Image
+                  src={"/brand-profile-icon2.png"}
+                  width={24}
+                  height={24}
+                  alt="icon"
+                  className="w-5 sm:w-6 h-5 sm:h-6"
+                />
+              </Link>
+              <Link
+                href={"/edit-profile-creator"}
+                className="border-[#2D262D] bg-[#F5F4F0] p-2 gap-5 rounded-lg"
+              >
+                <Image
+                  src={"/brand-profile-icon3.png"}
+                  width={24}
+                  height={24}
+                  alt="icon"
+                  className="w-5 sm:w-6 h-5 sm:h-6"
+                />
+              </Link>
+              <Link
+                href="/products"
+                className="flex sm:hidden flex-row items-center gap-2 text-xs sm:text-base bg-[#CA7FFE] border-[1px] border-[#2D262D] px-3 sm:px-5 py-2 sm:py-3 rounded-lg text-white font-bold"
+              >
+                Бүтээгдэхүүн үзэх
+                <Image
+                  src={"/arrow-right-icon.png"}
+                  width={14}
+                  height={14}
+                  alt="arrow"
+                  className="w-[9px] h-[9px] sm:w-[14px] sm:h-[14px]"
+                />
+              </Link>
+            </div>
+          </div>
+          {listContentGalleryData && listContentGalleryData?.Data != null ? (
+            <ContentGallery contentsGallery={listContentGalleryData?.Data} />
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default page;
