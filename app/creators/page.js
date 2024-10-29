b6e47002b73d95fdf15c87b355c85d88dbdacb1c
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2024-10-29 17:23:22 +0800

    updated

--- app/creators/page.js
+++ app/creators/page.js
@@ -78,7 +78,7 @@ function creators() {
               </span>
             </div>
           </div>
-          <div className="rounded-2xl px-6 py-8 sm:p-14 border-[1px] border-[#2D262D] bg-[#F5F4F0] mt-12 sm:mt-20 flex flex-col lg:flex-row gap-6 lg:gap-0 justify-between">
+          <div className="rounded-2xl px-6 py-8 sm:p-14 border-[1px] border-[#2D262D] bg-[#F5F4F0] mt-12 sm:mt-20 flex flex-col lg:flex-row items-end gap-6 lg:gap-0 justify-between">
             <div className="flex flex-col gap-4">
               <Image
                 loading="lazy"
@@ -87,11 +87,12 @@ function creators() {
                 height={29}
                 alt="geni-creator-logo"
               />
-              <span className="text-2xl font-bold text-[#6F6F6F]">
-                Дараагийн хөтөлбөр зарлагдаагүй байна.
+              <span className="text-lg sm:text-2xl font-bold text-[#6F6F6F]">
+                Онлайн хөтөлбөрт хамрагдан суралцангаа бодит туршлага
+                хуримтлуулан хэрэглэгчээс бүтээгч болоорой.
               </span>
             </div>
-            <div className="w-full lg:max-w-[371px] h-[84px] shadow-2xl rounded-xl border-[1px] border-[#2D262D] bg-[#9c44da]">
+            <div className="w-full relative lg:max-w-[371px] h-[84px] shadow-2xl rounded-xl border-[1px] border-[#2D262D] bg-[#9c44da]">
               <a
                 target="_blank"
                 href="https://docs.google.com/forms/d/e/1FAIpQLSdrBB6Hjs2cnrLWe4Z48HHCr56RcRFXvK8klYB5VifU-YckYw/viewform"
