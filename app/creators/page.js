a0d3133354e71218b36a4c4c0f3a6b47914f7aa3
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2024-11-18 14:20:50 +0800

    hidden unused button

--- app/creators/page.js
+++ app/creators/page.js
@@ -143,7 +143,7 @@ function creators() {
                 Онлайн хөтөлбөрт хамрагдан суралцангаа бодит туршлага
                 хуримтлуулан хэрэглэгчээс бүтээгч болоорой.
               </span>
-              <div className="relative w-full lg:max-w-[371px] h-[84px] shadow-2xl rounded-xl border-[1px] border-[#2D262D] bg-[#9c44da]">
+              <div className="hidden lg:block relative w-full lg:max-w-[371px] h-[84px] shadow-2xl rounded-xl border-[1px] border-[#2D262D] bg-[#9c44da]">
                 <a
                   target="_blank"
                   href="https://docs.google.com/forms/d/e/1FAIpQLSdrBB6Hjs2cnrLWe4Z48HHCr56RcRFXvK8klYB5VifU-YckYw/viewform"
@@ -167,7 +167,7 @@ function creators() {
               height={385}
               alt="become-creator"
             />
-            <div className="relative mt-9 w-full lg:max-w-[371px] h-[84px] shadow-2xl rounded-xl border-[1px] border-[#2D262D] bg-[#9c44da]">
+            <div className="block lg:hidden relative mt-9 w-full lg:max-w-[371px] h-[84px] shadow-2xl rounded-xl border-[1px] border-[#2D262D] bg-[#9c44da]">
               <a
                 target="_blank"
                 href="https://docs.google.com/forms/d/e/1FAIpQLSdrBB6Hjs2cnrLWe4Z48HHCr56RcRFXvK8klYB5VifU-YckYw/viewform"
