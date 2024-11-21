f5c921dec228cd301d17436b13c5fdbb39c89f05
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2024-11-21 17:17:18 +0800

    added fonts

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Head from "next/head";
+import localFont from "next/font/local";
 
 import "/public/fonts/font/stylesheet.css";
 import { store } from "./store";
@@ -10,10 +11,30 @@ import { Provider } from "react-redux";
 import { Toaster } from "react-hot-toast";
 import { UserInfoProvider } from "./context/UserInfoContext";
 
+const mabryPro = localFont({
+  src: [
+    {
+      path: "../public/fonts/font/MabryPro-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/font/MabryPro-Medium.ttf",
+      weight: "500",
+    },
+    {
+      path: "../public/fonts/font/MabryPro-Bold.ttf",
+      weight: "700",
+    },
+  ],
+  display: "swap",
+  variable: "--font-mabry-pro",
+});
+
 export default function RootLayout({ children }) {
   return (
     <Provider store={store}>
-      <html lang="en">
+      <html lang="en" className={mabryPro.className}>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta property="og:image" content="/meta-logo.png" />
         <Head>
