ae12c40e216781b74b6dd1c9ec2c97db51d9e261
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2024-11-28 18:15:33 +0800

    added search filter for public brand lists

--- app/all-brands/AllBrands.js
+++ app/all-brands/AllBrands.js
@@ -1,21 +1,42 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { useGetPublicBrandListQuery } from "../services/service";
 
 function AllBrands() {
+  const [searchQuery, setSearchQuery] = useState("");
   const {
     data: getPublicBrandListData,
     error: getPublicBrandListError,
     isLoading: getPublicBrandListLoading,
   } = useGetPublicBrandListQuery();
+
+  const filteredBrands = getPublicBrandListData?.Data?.filter((brand) =>
+    brand?.Name?.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   return (
     <div className="flex flex-col gap-6">
       <span className="text-[#6F6F6F] text-base sm:text-2xl">
         Бүх Geni брэндүүд / {getPublicBrandListData?.Data?.length}
       </span>
+      <div className="bg-[#F2F2F2] rounded-xl w-full flex flex-row items-center justify-between gap-2 px-4 py-3 sm:pl-10 sm:pr-6 sm:py-6">
+        <input
+          className="bg-inherit outline-none w-full placeholder:text-[#6F6F6F] placeholder:text-sm sm:placeholder:text-base"
+          placeholder="Хайлт хийх..."
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <Image
+          src={"/search-icon.png"}
+          width={24}
+          height={24}
+          alt="search"
+          className="w-5 h-5 sm:w-6 sm:h-6"
+        />
+      </div>
       <div className=" grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-[10px] sm:gap-4">
-        {getPublicBrandListData?.Data.map((brand, id) => {
+        {filteredBrands?.map((brand, id) => {
           const instagramLink = brand?.Socials?.find(
             (channel) => channel.Name === "Instagram"
           )?.SocialAddress;
