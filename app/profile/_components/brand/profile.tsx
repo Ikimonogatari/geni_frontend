1220a6c79047125762e44c5f668603aecd77da6a
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2025-02-10 11:31:06 +0800

    added show credit point on brand profile

--- app/profile/_components/brand/profile.tsx
+++ app/profile/_components/brand/profile.tsx
@@ -290,7 +290,7 @@ function BrandProfile() {
                   userInfo={getUserInfoData}
                 />
               )}
-              <div className="w-full sm:w-auto flex flex-row items-center justify-between sm:justify-normal sm:items-start sm:flex-col gap-2 sm:gap-4">
+              <div className="w-full sm:w-auto flex flex-row items-center justify-between sm:justify-normal sm:items-center sm:flex-col gap-2 sm:gap-4">
                 <Link
                   href={"/wallet"}
                   className="flex flex-row items-center gap-2 p-2 sm:px-4 sm:py-2 bg-[#4FB755] rounded-lg"
