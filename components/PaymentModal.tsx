3ecf5e55a3b86e2e92760e48669b593d65355f78
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2025-02-19 23:10:12 +0800

    fixed payment modal state

--- components/PaymentModal.tsx
+++ components/PaymentModal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import Image from "next/image";
 import {
   Dialog,
@@ -14,6 +14,7 @@ import {
   useSubscribePlanMutation,
 } from "@/app/services/service";
 import toast from "react-hot-toast";
+import { ClipLoader } from "react-spinners";
 
 interface PaymentModalProps {
   selectedPackageId: number;
@@ -48,7 +49,7 @@ const PaymentModal: React.FC<PaymentModalProps> = ({
   useEffect(() => {
     if (subscribePlanSuccess) {
       setTxId(subscribePlanData?.UserTxnId || null);
-      setPaymentDialogOpen(true); // Open dialog when subscription succeeds
+      setPaymentDialogOpen(true);
     }
     if (subscribePlanError) {
       //@ts-ignore
@@ -83,9 +84,10 @@ const PaymentModal: React.FC<PaymentModalProps> = ({
   };
 
   return (
-    <Dialog open={isPaymentDialogOpen} onOpenChange={setPaymentDialogOpen}>
-      <DialogTrigger
+    <>
+      <button
         onClick={handleSubscription}
+        disabled={subscribePlanLoading}
         className={`flex ml-auto whitespace-nowrap flex-row text-xs sm:text-base items-center gap-2 bg-[#4D55F5] border-[1px] border-[#2D262D] px-3 sm:px-5 py-2 sm:py-3 rounded-lg text-white font-bold`}
       >
         Үргэлжлүүлэх
@@ -96,59 +98,86 @@ const PaymentModal: React.FC<PaymentModalProps> = ({
           alt="arrow"
           className="w-[10px] h-[10px]"
         />
-      </DialogTrigger>
-      {/* @ts-ignore */}
-      <DialogContent className={"max-w-lg rounded-3xl w-full"}>
-        {!isPaymentSuccess && (
-          //@ts-ignore
-          <DialogHeader>
-            {/* @ts-ignore */}
-            <DialogTitle className="text-3xl">QPay Payment</DialogTitle>
-          </DialogHeader>
-        )}
-        {subscribePlanSuccess && subscribePlanData ? (
-          !isPaymentSuccess ? (
-            <div className="flex flex-col gap-6">
-              <Image
-                src={`data:image/png;base64,${subscribePlanData?.QrImage}`}
-                width={394}
-                height={394}
-                alt="dummy-qr"
-                className="aspect-square w-full rounded-2xl"
-              />
-
-              <button
-                onClick={handleCheckPayment}
-                className="bg-[#4D55F5] text-white font-bold border-[1px] border-[#2D262D] rounded-lg w-full text-center py-4"
-              >
-                Төлбөр шалгах
-              </button>
-            </div>
-          ) : (
-            <div className="flex flex-col gap-6">
-              <Image
-                src={"/payment-success.png"}
-                width={353}
-                height={271}
-                className=""
-                alt=""
-              />
-              <span className="uppercase font-bold text-[#4FB755] text-4xl sm:text-5xl text-center">
-                АМЖИЛТТАЙ ТӨЛӨГДЛӨӨ
-              </span>
-              <DialogClose
-                onClick={handleCloseDialog}
-                className="mt-24 bg-[#4D55F5] text-white font-bold border-[1px] border-[#2D262D] rounded-lg w-full text-center py-4"
-              >
-                Баярлалаа
-              </DialogClose>
-            </div>
-          )
-        ) : (
-          <></>
-        )}
-      </DialogContent>
-    </Dialog>
+      </button>
+      <Dialog open={isPaymentDialogOpen} onOpenChange={setPaymentDialogOpen}>
+        {/* @ts-ignore */}
+        <DialogContent className={"max-w-lg rounded-3xl w-full"}>
+          {!isPaymentSuccess && (
+            //@ts-ignore
+            <DialogHeader>
+              {/* @ts-ignore */}
+              <DialogTitle className="text-3xl">QPay Payment</DialogTitle>
+            </DialogHeader>
+          )}
+          <div className="flex justify-center items-center w-full">
+            {subscribePlanSuccess && subscribePlanData ? (
+              !isPaymentSuccess ? (
+                <div className="flex flex-col gap-6 w-full">
+                  {!subscribePlanLoading && subscribePlanData ? (
+                    <Image
+                      src={`data:image/png;base64,${subscribePlanData?.QrImage}`}
+                      width={394}
+                      height={394}
+                      alt="dummy-qr"
+                      className="aspect-square w-full rounded-2xl"
+                    />
+                  ) : (
+                    <div className="w-[394px] h-[394px] rounded-2xl aspect-square bg-primary-bg flex justify-center items-center">
+                      <ClipLoader
+                        loading={subscribePlanLoading}
+                        aria-label="Loading Spinner"
+                        data-testid="loader"
+                        className="aspect-[9/16] w-full h-full sm:w-[272px] rounded-2xl"
+                        size={50}
+                      />
+                    </div>
+                  )}
+                  <button
+                    onClick={handleCheckPayment}
+                    className="bg-[#4D55F5] text-white font-bold border-[1px] border-[#2D262D] rounded-lg w-full text-center py-4"
+                  >
+                    Төлбөр шалгах
+                  </button>
+                </div>
+              ) : (
+                <div className="flex flex-col gap-6">
+                  <Image
+                    src={"/payment-success.png"}
+                    width={353}
+                    height={271}
+                    className=""
+                    alt=""
+                  />
+                  <span className="uppercase font-bold text-[#4FB755] text-4xl sm:text-5xl text-center">
+                    АМЖИЛТТАЙ ТӨЛӨГДЛӨӨ
+                  </span>
+                  <DialogClose
+                    onClick={handleCloseDialog}
+                    className="mt-24 bg-[#4D55F5] text-white font-bold border-[1px] border-[#2D262D] rounded-lg w-full text-center py-4"
+                  >
+                    Баярлалаа
+                  </DialogClose>
+                </div>
+              )
+            ) : (
+              <>
+                {!subscribePlanError && (
+                  <div className="w-[394px] h-[394px] rounded-2xl aspect-square bg-primary-bg flex justify-center items-center">
+                    <ClipLoader
+                      loading={subscribePlanLoading}
+                      aria-label="Loading Spinner"
+                      data-testid="loader"
+                      className="aspect-[9/16] w-full h-full sm:w-[272px] rounded-2xl"
+                      size={50}
+                    />
+                  </div>
+                )}
+              </>
+            )}
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
