9b8db89673f13dabee47bce77e93a7c4691465e7
dulguun <dulguun.tuguldur11@gmail.com>
2025-04-07 09:31:35 +0800

    show return deliverty button

--- components/content-progress/ContentProgressModal.tsx
+++ components/content-progress/ContentProgressModal.tsx
@@ -651,8 +651,7 @@ const ContentProgressModalContent: React.FC<
           )} */}
         {status === STATUS_LIST.DeliverySuccess && (
           <div className="flex flex-col gap-2 border-t pt-2">
-            {/* Бүтээгдэхүүн буцаах button админ талд хийгдтэл нуусан */}
-            {/* <button
+            <button
               type="button"
               onClick={() =>
                 openReturnSection
@@ -664,7 +663,7 @@ const ContentProgressModalContent: React.FC<
                 } px-3 sm:px-5 py-2 rounded-lg text-white font-bold`}
             >
               Бүтээгдэхүүн буцаах
-            </button> */}
+            </button>
             <button
               type="button"
               disabled={isReceivedProductLoading || isReceivedProductSuccess}
