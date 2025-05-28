d2c1e00c3fba1cd988c99bf74d78504fffc5accc
dulguun <dulguun.tuguldur11@gmail.com>
2025-05-28 10:38:48 +0800

    brand product restock allow negative

--- app/profile/_components/brand/BrandProducts.tsx
+++ app/profile/_components/brand/BrandProducts.tsx
@@ -20,7 +20,7 @@ function BrandProducts({ brandProducts, brandData, isLoading }) {
   const [requestStatus, setRequestStatus] = useState("");
 
   const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count > 0 ? count - 1 : 0);
+  const decrement = () => setCount(count - 1);
 
   const [
     deleteProduct,
