0976d8cd07ee760e625e7bcf6ac8b31b6af90f6e
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2025-05-31 17:35:18 +0800

    set video to nodownload on receive content modal

--- app/profile/_components/brand/ContentReceiveModal.tsx
+++ app/profile/_components/brand/ContentReceiveModal.tsx
@@ -88,32 +88,33 @@ function ContentReceiveModal({
           <div className="flex flex-col gap-4 w-full h-full">
             <div className="flex flex-row items-center justify-between">
               <span className="text-base font-semibold">Контент</span>
-              {videoFileId && (
-                <a
-                  href={getDownloadUrl(videoFileId)}
-                  download="video.mp4"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center gap-1 text-[#4D55F5] text-sm font-medium"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
+              {videoFileId &&
+                !p.CurrentStepName?.String?.includes("BrandConfirming") && (
+                  <a
+                    href={getDownloadUrl(videoFileId)}
+                    download="video.mp4"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-1 text-[#4D55F5] text-sm font-medium"
                   >
-                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-                    <polyline points="7 10 12 15 17 10"></polyline>
-                    <line x1="12" y1="15" x2="12" y2="3"></line>Т
-                  </svg>
-                  Татах
-                </a>
-              )}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                      <polyline points="7 10 12 15 17 10"></polyline>
+                      <line x1="12" y1="15" x2="12" y2="3"></line>
+                    </svg>
+                    Татах
+                  </a>
+                )}
             </div>
             {contentVideo ? (
               <div className="relative">
